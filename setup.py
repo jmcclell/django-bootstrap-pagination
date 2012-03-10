14e96cd2a975f116ff605c6b4642c61de3c4aabb
Jason <jason@jasonmcclellan.net>
2012-03-10 08:42:40 -0500

    Updated version number for distribution via pip. Added build artifacts to gitignore.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='django-bootstrap-pagination',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
     packages=find_packages(),
