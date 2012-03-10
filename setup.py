7e78e9a311f65fd9c161bc89442c003557a4a1d5
Jason <jason@jasonmcclellan.net>
2012-03-10 08:57:59 -0500

    Added additional packaging information.

--- setup.py
+++ setup.py
@@ -2,16 +2,27 @@
 from distutils.core import setup
 from setuptools import find_packages
 
+readme = open('README.markdown', 'r')
+readme_text = readme.read()
+readme.close()
+
 setup(
     name='django-bootstrap-pagination',
     version='0.1.1',
+    keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
     packages=find_packages(),
     url='https://github.com/jmcclell/django-bootstrap-pagination',
     license='MIT licence, see LICENCE',
     description='Render Django Page objects as Bootstrap Pagination compatible HTML',
-    long_description=open('README.markdown').read(),
+    long_description=readme_text,
     zip_safe=False,
-    include_package_data=True
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",        
+    ]
 )
