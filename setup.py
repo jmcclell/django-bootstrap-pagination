6f2aba81085255d8cbaee8764398e30335cefc7f
Jason <jason@jasonmcclellan.net>
2012-03-10 09:10:15 -0500

    Adding ReStructured Text README which is compatible with pypi.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-readme = open('README.markdown', 'r')
+readme = open('README.rst', 'r')
 readme_text = readme.read()
 readme.close()
 
 setup(
     name='django-bootstrap-pagination',
-    version='0.1.1',
+    version='0.1.3',
     keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
@@ -16,7 +16,7 @@ setup(
     url='https://github.com/jmcclell/django-bootstrap-pagination',
     license='MIT licence, see LICENCE',
     description='Render Django Page objects as Bootstrap Pagination compatible HTML',
-    long_description=readme_text,
+    long_description=readme_text.encode('UTF-8'),
     zip_safe=False,
     include_package_data=True,
     classifiers=[
