253e96b333dfbe605747fe69189eeace21884a85
Jason <jason@jasonmcclellan.net>
2012-03-10 07:29:08 -0500

    Initial import

---
+++ README.markdown
@@ -0,0 +1,7 @@
+# Django Bootstrap Pagination
+
+This application serves to make using Twitter's Bootstrap Pagination styles
+work seamlessly with Django Page objects.
+
+# Usage
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-bootstrap-pagination',
+    version='0.1.0',
+    author=u'Jason McClellan',
+    author_email='jason@jasonmccllelan.net',
+    packages=find_packages(),
+    url='https://github.com/jmcclell/django-bootstrap-pagination',
+    license='MIT licence, see LICENCE',
+    description='Render Django Page objects as Bootstrap Pagination compatible HTML',
+    long_description=open('README.markdown').read(),
+    zip_safe=False,
+    include_package_data=True
+)
