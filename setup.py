0aa551e7116042c065284f1eadbb1569328fc7e8
Jason <jason@jasonmcclellan.net>
2012-03-20 02:43:45 -0400

    Incremetinting version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ readme.close()
 
 setup(
     name='django-bootstrap-pagination',
-    version='0.1.5',
+    version='0.1.7',
     keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
