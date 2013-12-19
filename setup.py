e220605709f6024163d24875d8a80f222592f76f
Jason McClellan <jason@jasonmcclellan.net>
2013-12-19 07:43:04 -0500

    Bumped version

--- setup.py
+++ setup.py
@@ -8,19 +8,19 @@ readme.close()
 
 setup(
     name='django-bootstrap-pagination',
-    version='1.0',
+    version='1.5.0',
     keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
     packages=find_packages(),
     url='https://github.com/jmcclell/django-bootstrap-pagination',
     license='MIT licence, see LICENCE',
-    description='Render Django Page objects as Bootstrap 2.x Pagination compatible HTML',
+    description='Render Django Page objects as Bootstrap 3.x Pagination compatible HTML',
     long_description=readme_text.encode('UTF-8'),
     zip_safe=False,
     include_package_data=True,
     classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 3 - Alpha",
         "Framework :: Django",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",        
