bc6d4f060b36418d7dc57ee4068e705b9bd28678
Koert van der Veer <koert@ondergetekende.nl>
2016-03-01 20:35:25 +0100

    Prepare for 1.6.1 on pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'rb') as readme:
 
 setup(
     name='django-bootstrap-pagination',
-    version='1.6.0',
+    version='1.6.1',
     keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
