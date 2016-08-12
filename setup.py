3ba0b6ad8591b98594a6246004179688c5a7e5a2
Jason McClellan <jason@jasonmcclellan.io>
2016-08-11 20:56:56 -0400

    Preparing 1.6.2 release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'rb') as readme:
 
 setup(
     name='django-bootstrap-pagination',
-    version='1.6.1',
+    version='1.6.2',
     keywords="django bootstrap pagination templatetag",
     author=u'Jason McClellan',
     author_email='jason@jasonmccllelan.net',
